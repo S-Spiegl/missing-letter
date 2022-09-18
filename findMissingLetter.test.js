ce5563e2e24322f74dace5935bf6c14978fa7335
S-Spiegl <steven.spiegl@gmail.com>
2022-09-18 16:33:54 +0100

    all tests passing

--- findMissingLetter.test.js
+++ findMissingLetter.test.js
@@ -4,7 +4,10 @@ describe('findMissingLetter', () => {
   it('finds missing letter', () => {
     expect(findMissingLetter(['a','c'])).toEqual('b')
   })  
-  // it('finds missing letter', () => {
-  //   expect(findMissingLetter(['a','b','c','d','f'])).toEqual('e')
-  // })  
+  it('finds missing letter longer', () => {
+    expect(findMissingLetter(['a','b','c','d','f'])).toEqual('e')
+  })  
+  it('finds missing letter capitalized', () => {
+    expect(findMissingLetter(['O','Q','R','S'])).toEqual('P')
+  })
 })
