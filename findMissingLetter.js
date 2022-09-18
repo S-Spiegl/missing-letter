dbca6c4c58099a7d5b9828d7c534165aa7caefb9
S-Spiegl <steven.spiegl@gmail.com>
2022-09-18 16:34:15 +0100

    removed logs

--- findMissingLetter.js
+++ findMissingLetter.js
@@ -3,8 +3,6 @@ function findMissingLetter(array)
   for(let i = 1; i < array.length; i++){
     const prev = array[i - 1].charCodeAt();
     const current = array[i].charCodeAt();
-    console.log(prev)
-    console.log(current)
     if(current-prev != 1){
       return String.fromCharCode(prev+1)
     }
