5c54769917d8afe9120ec5e011371bc7be65d29a
S-Spiegl <steven.spiegl@gmail.com>
2022-09-18 16:32:33 +0100

    basic test passing;

--- findMissingLetter.js
+++ findMissingLetter.js
@@ -1,6 +1,14 @@
 function findMissingLetter(array)
 {
-  return 'e';
+  for(let i = 1; i < array.length; i++){
+    const prev = array[i - 1].charCodeAt();
+    const current = array[i].charCodeAt();
+    console.log(prev)
+    console.log(current)
+    if(current-prev != 1){
+      return String.fromCharCode(prev+1)
+    }
+  }
 }
 
-module.exports = findMissingLetter;
+  module.exports = findMissingLetter;
--- findMissingLetter.test.js
+++ findMissingLetter.test.js
@@ -2,6 +2,9 @@ const findMissingLetter = require('./findMissingLetter')
 
 describe('findMissingLetter', () => {
   it('finds missing letter', () => {
-    expect(findMissingLetter(['a','b','c','d','f'])).toEqual('e')
+    expect(findMissingLetter(['a','c'])).toEqual('b')
   })  
+  // it('finds missing letter', () => {
+  //   expect(findMissingLetter(['a','b','c','d','f'])).toEqual('e')
+  // })  
 })
